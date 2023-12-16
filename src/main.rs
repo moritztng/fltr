caf46ee3382836e12f1ed684cdf808cf81fedeac
Moritz <moritz.thuening@gmail.com>
2023-12-16 13:01:34 +0000

    add cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use clap::Parser;
 use llamars::generate;
 use std::error::Error;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    weights: String,
+
+    #[arg(short, long)]
+    prompt: String,
+
+    #[arg(short, long, default_value_t = 256)]
+    length: usize,
+}
+
 fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    generate("weights.bin".into(), "Once upon a time".into(), 256, true)?;
+    let args = Args::parse();
+    generate(args.weights, args.prompt, args.length, true)?;
     Ok(())
 }
