86ae904ca53f63115a68054847ea128d06014909
Moritz Thüning <moritz.thuening@gmail.com>
2024-01-07 10:35:02 +0000

    change python3 to env/bin/python3

--- bots/start.sh
+++ bots/start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 target/release/mixtral server &
-source env/bin/activate
-while true; do python3 bots/x_bot.py; sleep 60; done &
-python3 bots/fetch_arxiv.py
+while true; do env/bin/python3 bots/x_bot.py; sleep 60; done &
+env/bin/python3 bots/fetch_arxiv.py
 sleep 100
 sudo shudown -h now
