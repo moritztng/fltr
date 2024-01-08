95d13f360722d01a1f276b9b4d9a82c8e89e6be4
Moritz Thüning <moritz.thuening@gmail.com>
2024-01-08 23:58:02 +0000

    fix shutdown

--- bots/start.sh
+++ bots/start.sh
@@ -4,4 +4,4 @@ target/release/mixtral server &
 while true; do env/bin/python3 bots/x_bot.py; sleep 60; done &
 env/bin/python3 bots/fetch_arxiv.py
 sleep 100
-sudo shudown -h now
+sudo /usr/sbin/shutdown -h now
