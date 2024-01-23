9c438fc3cd0e4d71b5830055d56b1af06ad46953
Moritz Thüning <moritz.thuening@gmail.com>
2024-01-23 03:37:40 +0100

    single main.py file for x bot

--- bots/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-target/release/mixtral server &
-while true; do env/bin/python3 bots/x_bot.py; sleep 60; done &
-env/bin/python3 bots/fetch_arxiv.py
-sleep 100
-sudo /usr/sbin/shutdown -h now
